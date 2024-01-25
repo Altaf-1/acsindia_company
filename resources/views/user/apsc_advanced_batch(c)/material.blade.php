3208f360324b2e9e42f489753e3eab7a48463daf
Altaf-1 <hussainaltafali35@gmail.com>
2024-01-25 13:32:26 +0530

    new update

--- resources/views/user/apsc_advanced_batch(c)/material.blade.php
+++ resources/views/user/apsc_advanced_batch(c)/material.blade.php
@@ -104,6 +104,26 @@
                             <!-- single features -->
                             <div>
                                 <i class="base-color fab fa-leanpub fa-3x"></i>
+                                <h4>GEOGRAPHY</h4>
+                                <h6>(Revision)</h6>
+                                <form action="{{route('user.pdf-view.index')}}" method="post">
+                                        @csrf
+                                        <input type="hidden"
+                                               name="drive_link"
+                                               value="https://drive.google.com/file/d/1K800bCQ2zwL7T6nH1csKqmD3nOrDM7xO/">
+                                        <button type="submit" class="btn color-two button text-white">Download</button>
+                                    </form>
+                            </div>
+                        </div>
+                        <!-- end single features -->
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                        <!-- 3 -->
+                        <div class="single-features-light text-center">
+                            <!-- single features -->
+                            <div>
+                                <i class="base-color fab fa-leanpub fa-3x"></i>
                                 <h4>ETHICS</h4>
                                 <form action="{{route('user.pdf-view.index')}}" method="post">
                                         @csrf

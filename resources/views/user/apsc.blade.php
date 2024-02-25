8e3a8079c91f503767bf729340471a83d6768f08
Altaf Hussain <hussainaltafali35@gmail.com>
2024-02-25 14:48:16 +0530

    new

--- resources/views/user/apsc.blade.php
+++ resources/views/user/apsc.blade.php
@@ -143,6 +143,31 @@
              </div>
              @endif
 
+             @if ($course->title == 'GHY OFFLINE (M)')
+             <!--  Prelims (PYQ) -->
+             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mt-3">
+                 <div class="single-features-light text-center">
+                     <div>
+                         <i class="base-color fas fa-book fa-3x"></i>
+                         <h4>APSC PRACTICE TEST SERIES</h4>
+                         <a href="{{ route('testseriesquiz.index', [$course->title ,"APSC"]) }}"
+                             class=" btn color-two button text-white">VIEW</a>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mt-3">
+                 <div class="single-features-light text-center">
+                     <div>
+                         <i class="base-color fas fa-book fa-3x"></i>
+                         <h4>APSC FULL TEST SERIES</h4>
+                         <a href="{{ route('quiz.index', $course->title) }}"
+                             class=" btn color-two button text-white">VIEW</a>
+                     </div>
+                 </div>
+             </div>
+              
+             @endif
+
              @if ($course->title == 'Target 2024 (Offline)')
              <!--  Prelims (PYQ) -->
              <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mt-3">
